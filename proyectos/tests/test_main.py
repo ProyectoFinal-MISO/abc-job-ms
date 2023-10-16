5bd0fb566778271ad351a46282a5513ee3ac7961
Camilo <dc.perezc1@uniandes.edu.co>
2023-10-15 19:54:03 -0500

    cascaron de los microservicios

---
+++ proyectos/tests/test_main.py
@@ -0,0 +1,22 @@
+import json
+from unittest import TestCase
+from unittest.mock import patch
+
+from faker import Faker
+from faker.generator import random
+
+from application import application
+from modelos.modelos import db
+
+class TestUsuarios(TestCase):
+
+    def setUp(self):
+        self.data_factory = Faker()
+        self.client = application.test_client()
+        application.app_context().push()
+
+    def test_health(self):
+        solicitud_nuevo_usuario = self.client.get(
+            "/projects/ping",
+        )
+        self.assertEqual(solicitud_nuevo_usuario.status_code, 200)
