308e4d4209864a5551a0dd8359c336c9995db5ad
jhinariv <jhina.riv@gmail.com>
2023-10-23 23:00:50 -0500

    Agregando pruebas unitarias

---
+++ usuarios/tests/test_technical_resource.py
@@ -0,0 +1,89 @@
+import pytest
+from datetime import datetime, timezone
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from modelos.modelos import TypeIdentification, TechnicalResource, Genre, EducationLevel
+from application import application as app
+
+from utils.technical_resource import TechnicalResourceCreate
+
+def new_technical_resource():
+    return {
+        "personalInformation": {
+            "name": "Juan",
+            "lastName": "Perez",
+            "typeIdentification": TypeIdentification.CC,
+            "identification":  '123456789' + generate_string_random(5),
+            "age": 30,
+            "genre": Genre.MALE,
+            "phoneNumber": "1234567",
+            "mobileNumber": "1234567890",
+            "city": "Bogota",
+            "state": "Bogota",
+            "country": "Colombia",
+            "address": "Calle 123"
+        },
+        "academicInformation" : [
+            {
+                "schoolName": "Universidad Nacional",
+                "educationLevel": EducationLevel.MASTER,
+                "professionalSector": 1,
+                "startDate": datetime(2010, 1, 1, tzinfo=timezone.utc),
+                "endDate": datetime(2012, 1, 1, tzinfo=timezone.utc)
+            }
+        ],
+        "professionalExperience" : [
+            {
+                "companyName": "ABC",
+                "titleJob": "Desarrollador",
+                "startDate": datetime(2010, 1, 1, tzinfo=timezone.utc),
+                "endDate": datetime(2012, 1, 1, tzinfo=timezone.utc),
+                "details": "Desarrollador de software"
+            }
+        ],
+        "aditionalInformation" : {
+            "driverLicense": "abcdef",
+            "transferAvailability": True,
+            "vehicule": "abcdef"
+        },
+        "programmingLanguages" : [
+            {
+                "name": "Python",
+                "score":5
+            }
+        ],
+        "languages" : [
+            {
+                "language": 1,
+                "score": 5
+            }
+        ],
+        "personalSkills" : [
+            {
+                "name": "Trabajo en equipo",
+                "score": 5
+            }
+        ]
+    }
+
+def test_technical_resource_create():
+    # Llama a la función TechnicalResourceCreate
+    response = TechnicalResourceCreate(userId = 1, user_data = new_technical_resource())
+
+    assert response[1] == 201
+
+def test_technical_resource_create_identification_exits():
+    technical_resource = new_technical_resource()
+    # Llama a la función TechnicalResourceCreate
+    response = TechnicalResourceCreate(userId = 1, user_data = technical_resource)
+    response_2 = TechnicalResourceCreate(userId = 1, user_data = technical_resource)
+
+    assert response_2[1] == 400
+
+
+def generate_string_random(length):
+    import string
+    import random
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
---
+++ usuarios/tests/test_vista_employee.py
@@ -0,0 +1,218 @@
+import json
+from unittest import TestCase
+from unittest.mock import patch
+
+from faker import Faker
+from faker.generator import random
+
+from flask_jwt_extended import create_access_token
+
+from application import application
+from modelos.modelos import db, TypeIdentification, Employee
+from vistas.employees import VistaEmployee
+
+
+class TestEmployees(TestCase):
+
+    def setUp(self):
+        token = create_access_token(identity='JWT_SECRET_KEY')
+        self.headers = {'Authorization': 'Bearer ' + token}
+        self.data_factory = Faker()
+        self.client = application.test_client()
+        application.app_context().push()
+
+
+
+    def test_get_employee(self):
+        # Create a fake employee
+        employee = Employee(
+            userId=1,
+            name=self.data_factory.name(),
+            lastName=self.data_factory.last_name(),
+            typeIdentification=TypeIdentification.CC,
+            identification=self.data_factory.random_number(digits=9),
+            phoneNumber=self.data_factory.phone_number(),
+            mobileNumber=self.data_factory.phone_number(),
+            city=self.data_factory.city(),
+            state=self.data_factory.state(),
+            country=self.data_factory.country(),
+            address=self.data_factory.address()
+        )
+        db.session.add(employee)
+        db.session.commit()
+
+        # Make a request to get the employee
+        response = self.client.get(f"/user/employee/{employee.id}", headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_employee_with_non_integer_id(self):
+        # Make a request to get a employee with a non-integer id
+        response = self.client.get(f"/user/employee/abc", headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_nonexistent_employee(self):
+
+        # Make a request to get a nonexistent employee
+        response = self.client.get(f"/user/employee/999", headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_employee(self):
+        # Create a employee to delete
+        employee = Employee(
+            userId=1,
+            name=self.data_factory.name(),
+            lastName=self.data_factory.last_name(),
+            typeIdentification=TypeIdentification.CC,
+            identification=self.data_factory.random_number(digits=9),
+            phoneNumber=self.data_factory.phone_number(),
+            mobileNumber=self.data_factory.phone_number(),
+            city=self.data_factory.city(),
+            state=self.data_factory.state(),
+            country=self.data_factory.country(),
+            address=self.data_factory.address()
+        )
+        db.session.add(employee)
+        db.session.commit()
+
+        # Delete the employee
+        response = self.client.delete(f'/user/employee/{employee.id}', headers=self.headers)
+
+        # Check that the employee was deleted
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['message'], 'Employee deleted')
+
+    def test_delete_nonexistent_employee(self):
+        # Delete a nonexistent employee
+        response = self.client.delete('/user/employee/999', headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json['message'], 'Employee not exist')
+
+    def test_delete_employee_with_noninteger_id(self):
+        # Delete a employee with a non-integer ID
+        response = self.client.delete('/user/employee/abc', headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['message'], 'Employee id is not integer')
+
+    def test_put_employee(self):
+        # Create a employee to update
+        employee = Employee(
+            userId=1,
+            name=self.data_factory.name(),
+            lastName=self.data_factory.last_name(),
+            typeIdentification=TypeIdentification.CC,
+            identification=self.data_factory.random_number(digits=9),
+            phoneNumber=self.data_factory.phone_number(),
+            mobileNumber=self.data_factory.phone_number(),
+            city=self.data_factory.city(),
+            state=self.data_factory.state(),
+            country=self.data_factory.country(),
+            address=self.data_factory.address()
+        )
+        db.session.add(employee)
+        db.session.commit()
+
+        # Update the employee
+        updated_employee = {
+            'name': self.data_factory.name(),
+            'lastName': self.data_factory.last_name(),
+            'typeIdentification': "CC",
+            'identification': self.data_factory.numerify('##########'),
+            'phoneNumber': self.data_factory.numerify('##########'),
+            'mobileNumber': self.data_factory.numerify('##########'),
+            'city': self.data_factory.city(),
+            'state': self.data_factory.state(),
+            'country': self.data_factory.country(),
+            'address': self.data_factory.address(),
+        }
+        response = self.client.put(f'/user/employee/{employee.id}', json=updated_employee, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the employee was updated
+        response = self.client.get(f'/user/employee/{employee.id}', headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['name'], updated_employee['name'])
+        self.assertEqual(response.json['identification'], updated_employee['identification'])
+        self.assertEqual(response.json['phoneNumber'], updated_employee['phoneNumber'])
+        self.assertEqual(response.json['mobileNumber'], updated_employee['mobileNumber'])
+        self.assertEqual(response.json['city'], updated_employee['city'])
+        self.assertEqual(response.json['state'], updated_employee['state'])
+        self.assertEqual(response.json['country'], updated_employee['country'])
+        self.assertEqual(response.json['address'], updated_employee['address'])
+
+    def test_update_nonexistent_employee(self):
+        # Update the employee
+        updated_employee = {
+            'name': self.data_factory.name(),
+            'lastName': self.data_factory.last_name(),
+            'typeIdentification': "CC",
+            'identification': self.data_factory.numerify('##########'),
+            'phoneNumber': self.data_factory.numerify('##########'),
+            'mobileNumber': self.data_factory.numerify('##########'),
+            'city': self.data_factory.city(),
+            'state': self.data_factory.state(),
+            'country': self.data_factory.country(),
+            'address': self.data_factory.address(),
+        }
+        # Update a nonexistent employee
+        response = self.client.put(f'/user/employee/999', json=updated_employee, headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json['message'], 'Employee not exist')
+
+    def test_update_employee_with_noninteger_id(self):
+        # Update the employee
+        updated_employee = {
+            'name': self.data_factory.name(),
+            'lastName' : self.data_factory.last_name(),
+            'typeIdentification': "CC",
+            'identification': self.data_factory.numerify('##########'),
+            'phoneNumber': self.data_factory.numerify('##########'),
+            'mobileNumber': self.data_factory.numerify('##########'),
+            'city': self.data_factory.city(),
+            'state': self.data_factory.state(),
+            'country': self.data_factory.country(),
+            'address': self.data_factory.address(),
+        }
+        # Update a nonexistent employee
+        response = self.client.put(f'/user/employee/abc', json=updated_employee, headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['message'], 'Employee id is not integer')
+
+    def test_update_employee_with_field_missing(self):
+        # Update the employee
+        updated_employee = {
+            'name': self.data_factory.name(),
+            'lastName' : self.data_factory.last_name(),
+            'identification': self.data_factory.numerify('##########'),
+            'phoneNumber': self.data_factory.numerify('##########'),
+            'mobileNumber': self.data_factory.numerify('##########'),
+            'city': self.data_factory.city(),
+            'state': self.data_factory.state(),
+            'country': self.data_factory.country(),
+            'address': self.data_factory.address(),
+        }
+        # Update a nonexistent employee
+        response = self.client.put(f'/user/employee/abc', json=updated_employee, headers=self.headers)
+
+        # Check that the response is correct
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['message'], 'Field is missing')
+
+def generate_string_random(length):
+    import string
+    import random
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
